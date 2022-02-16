523ee2524c692a873760879acfb0466851a495c9
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-02-17 00:00:39 +0200

    add

---
+++ src/components/FavList.js
@@ -0,0 +1,58 @@
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { useEffect, useState } from "react";
+
+function FavList() {
+  const [favListData, setFavListDta] = useState();
+
+  async function getDataFromDB() {
+    const response = await fetch(`${process.env.REACT_APP_SERVER}/getMovies`);
+    const data = await response.json();
+    setFavListDta(data);
+  }
+  useEffect(() => {
+    getDataFromDB();
+  }, []);
+  async function handleDelete(id) {
+    const response = await fetch(
+      `${process.env.REACT_APP_SERVER}/DELETE/${id}`,
+      {
+        method: "DELETE",
+      }
+    );
+    if (response.status === 204) {
+      getDataFromDB();
+    }
+  }
+  return (
+    <>
+      <h1>My Favorite Movies</h1>
+      {favListData &&
+        favListData.map((movie) => {
+          return (
+            <>
+              <Card>
+                <Card.Img
+                  variant="top"
+                  src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                />
+                <Card.Body>
+                  <Card.Title>{movie.title}</Card.Title>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      handleDelete(movie.id);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </Card.Body>
+              </Card>
+            </>
+          );
+        })}
+    </>
+  );
+}
+
+export default FavList;
--- src/components/ModalMovie.js
+++ src/components/ModalMovie.js
@@ -2,8 +2,46 @@ import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { Card } from "react-bootstrap";
+import { useRef } from "react";
+import axios from "axios";
 
 export default function MovieModal(props) {
+  const commintRrf = useRef();
+  function handleComment(e) {
+    e.preventDefault();
+    const comment = commintRrf.current.value;
+    const newMovie = { ...props.chosenMovie, comment };
+    props.newdata(newMovie, props.chosenMovie.id);
+  }
+
+  async function handleAddFav(e, movie) {
+    e.preventDefault();
+    const dataToBeSent = {
+      title: movie.title,
+      release_date: movie.release_date,
+      poster_path: movie.poster_path,
+      overview: movie.overview,
+      comment: movie.comment,
+    };
+    console.log(dataToBeSent);
+    const url = `https://js-prp-course.herokuapp.com/addMovie/`;
+    // axios
+    //   .post(url, dataToBeSent)
+    //   .then((data) => {
+    //     console.log(data.data);
+    //   })
+    //   .catch((error) => console.log(error));
+    const response = await fetch(url, {
+      method: `POST`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataToBeSent),
+    });
+    const data = await response.json();
+    console.log(response.status);
+    console.log(data);
+  }
   return (
     <>
       <Modal show={props.show} onHide={props.MovieModalhandleClose}>
@@ -17,22 +55,39 @@ export default function MovieModal(props) {
             `${props.chosenMovie.poster_path}`
           }
         />
+        <p>
+          User Comment:{" "}
+          {props.chosenMovie.comment
+            ? props.chosenMovie.comment
+            : "No Comment is added"}
+        </p>
         <Modal.Footer>
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Comment</Form.Label>
-              <Form.Control type="text" placeholder="Enter your comment" />
+              <Form.Control
+                ref={commintRrf}
+                type="text"
+                placeholder="Enter your comment"
+              />
               <Form.Text className="text-muted"></Form.Text>
             </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
+
+            <Button variant="primary" type="submit" onClick={handleComment}>
+              comment
             </Button>
           </Form>
           <Button variant="secondary" onClick={props.handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={props.handleClose}>
-            Save Changes
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={(e) => {
+              handleAddFav(e, props.chosenMovie);
+            }}
+          >
+            Add to Favorites
           </Button>
         </Modal.Footer>
       </Modal>
