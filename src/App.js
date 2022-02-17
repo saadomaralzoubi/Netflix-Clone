476b401b1d4a70dfdd89527d2d333092241e9be1
saadalzoubi <saad.alzoubi.1994@gmail.com>
2022-02-17 10:26:04 +0200

    aa

--- src/App.js
+++ src/App.js
@@ -4,29 +4,15 @@ import NavBar from "./components/Navbar";
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
 import FavList from "./components/FavList";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [favListData, setFavListDta] = useState();
-
-  async function getDataFromDB() {
-    const response = await fetch(`${process.env.REACT_APP_SERVER}/favorite`);
-    const data = await response.json();
-    setFavListDta(data);
-  }
-  useEffect(() => {
-    getDataFromDB();
-  }, []);
   return (
     <>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         const [favListData, setFavListDta] = useState();
-        <Route
-          path="/favList"
-          element={<FavList favListData={favListData} />}
-        />
+        <Route path="/favList" element={<FavList />} />
       </Routes>
     </>
   );
